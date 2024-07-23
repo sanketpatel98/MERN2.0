f50ad73409de2b3900d0d0ea6e457b3508bbec90
sanketpatel <patelsanketr98@gmail.com>
2024-07-23 16:25:41 -0400

    portfolio button added

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -2,9 +2,16 @@ import { Button } from "flowbite-react";
 import React from "react";
 
 export default function CallToAction() {
-  const handleClick = () => {
+  const handleClickResume = () => {
     window.open(
       "https://docs.google.com/document/d/1cidRkwkDNWiFWkWbsA24nqZC_HByINm8vBuOdx9YetU/edit?usp=sharing",
+      "_blank",
+      "noopener,noreferrer"
+    );
+  };
+  const handleClickPortfolio = () => {
+    window.open(
+      "https://sanketpatel98.github.io/portfolio",
       "_blank",
       "noopener,noreferrer"
     );
@@ -14,13 +21,22 @@ export default function CallToAction() {
       <div className="flex-1 justify-center flex flex-col">
         <h2 className="text-2xl">Want to learn more about Sanket?</h2>
         <p className="text-gray-500 my-2">Checkout the link below</p>
-        <Button
-          gradientDuoTone="purpleToPink"
-          className="rounded-tl-xl rounded-bl-none"
-          onClick={handleClick}
-        >
-          Resume
-        </Button>
+        <div className="flex flex-row justify-center gap-5">
+          <Button
+            gradientDuoTone="purpleToPink"
+            className="rounded-tl-xl"
+            onClick={handleClickResume}
+          >
+            Resume
+          </Button>
+          <Button
+            gradientDuoTone="purpleToPink"
+            className="rounded-tl-xl"
+            onClick={handleClickPortfolio}
+          >
+            Potfolio
+          </Button>
+        </div>
       </div>
       <div className="p-7 flex-1">
         <img src="https://miro.medium.com/v2/resize:fit:960/0*ngXgBNNdx6iiWP8q.png" />
