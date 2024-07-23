05befcbcc0e14f19c79df77b8e34a443be97c577
sanketpatel <patelsanketr98@gmail.com>
2024-07-23 10:46:43 -0400

    server proxy added to backend calls

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,8 +6,10 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target: 'http://localhost:3000',
-        secure: false
+        // target: 'http://localhost:3000',
+        target: 'https://mern-2-0-git-master-sanketpatel98s-projects.vercel.app',
+        secure: true,
+        changeOrigin: true
       }
     }
   },
