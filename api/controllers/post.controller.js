c810f9337ebf1ec68b71120f0d1cab8cd08466d2
sanketpatel <patelsanketr98@gmail.com>
2024-07-25 12:21:03 -0400

    srot direction query updated

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -30,7 +30,7 @@ export const getPosts = async (req, res, next) => {
   try {
     const startIndex = parseInt(req.query.startIndex) || 0;
     const limit = parseInt(req.query.limit) || 9;
-    const sortDirection = req.query.order === "asc" ? 1 : -1;
+    const sortDirection = req.query.sort.toString() == "asc" ? 1 : -1;
 
     const posts = await Post.find({
       ...(req.query.userId && { userId: req.query.userId }),
